7487c7fba31588d220a421a79ea8cf88a614211d
Andrea Crotti <andrea.crotti.0@gmail.com>
2023-03-07 14:50:50 +0000

    read from wikipedia

--- src/clojure_debugging/history.clj
+++ src/clojure_debugging/history.clj
@@ -1,4 +1,17 @@
-(ns clojure-debugging.history)
+(ns clojure-debugging.history
+  (:require
+   [clojure.string :as string]
+   [hato.client :as h]))
 
 (defn meaning-of-life []
-  (inc 42))
+  (->> "https://en.wikipedia.org/wiki/List_of_black_films_of_the_2010s"
+       h/get
+       :body
+       (re-seq #"\/.*\_\(film\)")
+       (map #(string/split %  #"/"))
+       (map last)
+       (map #(string/split % #"_"))
+       (map first)
+       (filter #(= 2 (count %)))
+       second
+       Integer/parseInt))
